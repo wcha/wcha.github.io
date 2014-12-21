b3d4a3dffd98e257ed20626759299c5492bf59f9
Christophe Pouliot <moxuz@users.noreply.github.com>
2014-12-20 20:33:50 -0800

    Update ApplicationTest.java

--- TestBedEssai2/app/src/androidTest/java/com/minecraftomega/myapplication/ApplicationTest.java
+++ TestBedEssai2/app/src/androidTest/java/com/minecraftomega/myapplication/ApplicationTest.java
@@ -3,7 +3,7 @@ package com.minecraftomega.myapplication;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-/**
+/** 
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
@@ -11,3 +11,4 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 }
+//hello aj
